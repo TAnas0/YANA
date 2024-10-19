ccc62e08d3bb8401f54ec99dcd848c3ecbc3399e
TAnas0 <anastiour@gmail.com>
2024-10-20 00:59:20 +0100

    feat(models): add NewsStory method to get text ready for analysis

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
+from analysis.clean import (
+    lowercase_text,
+    remove_extra_whitespace,
+    remove_html,
+    remove_punctuation,
+    remove_special_characters,
+    remove_stopwords,
+    lemmatize_text,
+    preprocess_text,
+)
+
 class NewsStory:
     def __init__(self, source: str, title: str, summary: str = None, content: str = None):
         self.source = source
@@ -19,4 +30,19 @@ class NewsStory:
     def __hash__(self):
         # Create a hash based on multiple attributes
         return hash((self.source, self.title))
+
+    def preprocess_text(self) -> str:
+        """
+        Preprocesses the text of the NewsStory instance by applying various cleaning and normalization steps
+        in preparation for AI/ML analysis
+        Relevant NewsStory fields are: title, summary, content.
+
+        Returns:
+            str: The cleaned and processed text, suitable for further analysis or comparison tasks.
+        """
+        title = "" if self.title is None else self.title
+        summary = "" if self.summary is None else self.summary
+        content = "" if self.content is None else self.content
+
+        return f"{preprocess_text(title)} {preprocess_text(summary)} {preprocess_text(content)}"
 
